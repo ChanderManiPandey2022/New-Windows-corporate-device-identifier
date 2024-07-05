acc589dd9a67f473cb3753ac56ef9270df6072be
ChanderManiPandey2022 <116195110+ChanderManiPandey2022@users.noreply.github.com>
2024-07-05 10:48:40 +0530

    Add files via upload

---
+++ Create_WindowsCorporateIdentifer_CSV.ps1
@@ -0,0 +1,17 @@
+﻿# Get computer system information
+$compSys = Get-WmiObject -Class Win32_ComputerSystem
+
+# Get BIOS information
+$biosInfo = Get-WmiObject -Class Win32_BIOS
+
+# Get the serial number
+$serialNumber = $biosInfo.SerialNumber
+
+# Combine the results into a single string without headers
+$info = "$($compSys.Manufacturer),$($compSys.Model),$serialNumber"
+
+# Define the file name using the serial number
+$fileName = "C:\WindowsCorporateIdentifer_$serialNumber.csv"
+
+# Write the data to a CSV file without headers
+$info | Out-File -FilePath $fileName -Encoding ASCII
